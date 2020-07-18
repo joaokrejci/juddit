d2f51ba109811ae6e867add02445e7745b59b69a
João Krejci <joao.krejci@gmail.com>
2020-07-17 20:44:36 -0400

    Adicionando boas vindas ao Juddit

--- src/br/uems/cursojava/Main.java
+++ src/br/uems/cursojava/Main.java
@@ -3,6 +3,6 @@ package br.uems.cursojava;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Bem vindo ao Juddit");
     }
 }
