9d8d34d37fda596b0aa7aefdffada0a2f794250d
Lucas Thiago Davalos Ortiz <lucasthidavort@gmail.com>
2020-07-20 20:54:25 -0400

    Implementando metodos abstratos para respostas, eliminando titulo da classe post, criando atributo respostas e getters que facam sentido.

--- src/br/uems/cursojava/Post.java
+++ src/br/uems/cursojava/Post.java
@@ -1,21 +1,31 @@
 package br.uems.cursojava;
 
+import java.util.ArrayList;
 import java.util.TreeSet;
 
 public abstract class Post {
+    /*
+    Este atributo foi removido pois os comentarios n vao ter titulos, só as perguntas. Implementar depois.
     private String titulo;
+    */
     private String texto;
     private final String IDusuario;
     private final String data;
+    private ArrayList<String> respostas;
     private TreeSet<String> IdUsuarios; //Pra depois
     private int votos = 0;
 
-    public Post(String titulo, String texto, Usuario usuario, String data){
-        this.titulo = titulo;
+    public Post(String texto, Usuario usuario, String data){
         this.texto = texto;
         this.IDusuario = usuario.getID();
         this.data = data;
+
+        // Pegar usuarios que ja votaram do banco de dados e dar eles para o tree set IdUsuarios depois
     }
+
+    public abstract String getRespostas();
+    public abstract String novaResposta();
+    public abstract void editaTexto(String novoTexto);
 
     public void voto(boolean voto, Usuario usuario){
         for(String ID: IdUsuarios) {
@@ -32,4 +42,12 @@ public abstract class Post {
 
     public int getVotos() {
         return this.votos;}
+
+    public String getData() {
+        return this.data;
+    }
+
+    public String getTexto() {
+        return this.texto;
+    }
 }
